9ef54b26f9f9d8ecbf235de17f4f3a1ef081ffae
kauan777 <kauan.silva@d2d.dev.br>
2023-03-16 10:48:04 -0300

    adding scripts

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -4,24 +4,6 @@ export default function Document() {
   return (
     <Html lang="pt-Br">
       <Head>
-        {/* <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=G-3CYGGC48KL"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'G-3CYGGC48KL');
-        </script>
-        <script
-          async
-          src="https://www.googletagmanager.com/gtag/js?id=AW-11084845242"
-        ></script>
-        <script>
-          window.dataLayer = window.dataLayer || []; function gtag()
-          {dataLayer.push(arguments)}
-          gtag('js', new Date()); gtag('config', 'AW-11084845242');
-        </script> */}
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
@@ -37,6 +19,24 @@ export default function Document() {
         <Main />
         <NextScript />
       </body>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-3CYGGC48KL"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'G-3CYGGC48KL');
+      </script>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=AW-11084845242"
+      ></script>
+      <script>
+        window.dataLayer = window.dataLayer || []; function gtag()
+        {dataLayer.push(arguments)}
+        gtag('js', new Date()); gtag('config', 'AW-11084845242');
+      </script>
     </Html>
   );
 }
